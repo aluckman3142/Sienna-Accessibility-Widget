88c9850f52f1736efa13c0741b1e400b1f336878
Dipak Sarkar <hello@dipaksarkar.in>
2024-07-05 15:00:21 +0530

    Fix Adjust Font Size

--- src/tools/adjustFontSize.ts
+++ src/tools/adjustFontSize.ts
@@ -1,24 +1,28 @@
-export default function adjustFontSize(multiply:number = 1) {
+export default function adjustFontSize(multiply: number = 1) {
     document
-        .querySelectorAll("h1,h2,h3,h4,h5,h6,p,a,dl,dt,li,ol,th,td,span,blockquote,.asw-text")
+        .querySelectorAll(
+            'h1,h2,h3,h4,h5,h6,p,a,dl,dt,li,ol,th,td,span,blockquote,.asw-text,.q-item__label,.text-title'
+        )
         .forEach((el: HTMLElement) => {
-            if(!el.classList.contains('material-icons') && !el.classList.contains('fa')) {
+            if (!el.classList.contains('material-icons') && !el.classList.contains('fa')) {
                 let orgFontSize = Number(el.getAttribute('data-asw-orgFontSize') ?? 0);
-    
-                if(!orgFontSize) {
-                    orgFontSize = parseInt(window.getComputedStyle(el).getPropertyValue('font-size'));
+
+                if (!orgFontSize) {
+                    orgFontSize = parseInt(
+                        window.getComputedStyle(el).getPropertyValue('font-size')
+                    );
                     el.setAttribute('data-asw-orgFontSize', String(orgFontSize));
                 }
-    
+
                 let newFontSize = orgFontSize * multiply;
-    
+
                 el.style['font-size'] = newFontSize + 'px';
             }
         });
-    
-    let $amount: HTMLDivElement = document.querySelector(".asw-amount");
 
-    if($amount) {
-        $amount.innerText = `${ (multiply * 100).toFixed(0) }%`
+    let $amount: HTMLDivElement = document.querySelector('.asw-amount');
+
+    if ($amount) {
+        $amount.innerText = `${(multiply * 100).toFixed(0)}%`;
     }
 }
